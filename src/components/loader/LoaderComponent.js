6d4ebc23407d78bac562b727c8c67ee1a225a054
Jesus Cordero <jc3356@hotmail.com>
2023-07-19 02:01:20 -0500

    images-hw-04-react-completed
    images-hw-04-react-completed

---
+++ src/components/loader/LoaderComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { LoaderComponentStyled } from "./LoaderComponentStyled";
+
+const LoaderComponent = () => {
+  return (
+    <LoaderComponentStyled>
+      <audio type="ThreeDots" color="#00BFFF" height={100} width={100} />
+    </LoaderComponentStyled>
+  );
+};
+
+export default LoaderComponent;
